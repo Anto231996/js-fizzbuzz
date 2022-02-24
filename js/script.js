073a3e93da27948be30268e975f12afa6c5a663a
Anto231996 <tony2396@hotmail.it>
2022-02-24 20:03:34 +0100

    Commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+//sintassi 1
+
+/* for(let num=1; num < 101; num++){
+    if ((num % 3 == 0) && (num % 5 == 0)){
+        console.log("FizzBizz");
+    }else if (num % 3 == 0){
+        console.log("Fizz");
+    }else if (num % 5 == 0){
+        console.log("Buzz");
+    }else{
+        console.log(num);
+    }
+} */
+
+
+//sintassi 2
+
+/* for (let num = 1; num < 101; num++) {
+    if ((num % 3 == 0) && (num % 5 == 0))
+    console.log("FizzBuzz");
+    else if (num % 3 == 0)
+    console.log("Fizz");
+    else if (num % 5 == 0)
+    console.log("Buzz");
+    else console.log(num);
+} */
+
+
+//sintassi 3
+
+/* for(let num=1; num < 101; num++){
+    if ((num % 3 == 0) && (num % 5 == 0)){console.log("FizzBizz");}
+    else if (num % 3 == 0){console.log("Fizz");}
+    else if (num % 5 == 0){console.log("Buzz");}
+    else{console.log(num)}
+} */
+
+
+//sintassi 4
+
+for(let num=1; num < 101; num++){
+    if ((num % 3 == 0) && (num % 5 == 0))console.log("FizzBizz");
+    else if (num % 3 == 0)console.log("Fizz");
+    else if (num % 5 == 0)console.log("Buzz");
+    else console.log(num);
+}
